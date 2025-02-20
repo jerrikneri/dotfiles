37216fb330da5e7fba7e87635c9914097e6f69e1
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2025-02-19 19:32:04 -0800

    extract duplicate code to script and source

--- .scripts/detect_os.sh
+++ .scripts/detect_os.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+source $SCRIPTS/get_os_variables.sh
+
+# Use os_name and os_version in this script
+echo "Operating System: $os_name"
+echo "OS Version: $os_version"
 # Determine the OS
-os_name=$(uname -s)
-os_version=$(lsb_release -c | awk '{print $2}' 2>/dev/null)  # For Ubuntu/Debian-based systems
+# if command -v uname &> /dev/null; then
+#   os_name=$(uname -s) # MacOS
+# fi
+
+# if command -v lsb_release &> /dev/null; then
+#   os_version=$(lsb_release -c | awk '{print $2}' 2>/dev/null) # Linux
+# fi
 
 case "$os_name" in
     Darwin)
---
+++ .scripts/get_os_variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get OS name
+if command -v uname &> /dev/null; then
+  os_name=$(uname -s)  # macOS or other Unix-like systems
+fi
+
+# Get OS version (for Linux systems)
+if command -v lsb_release &> /dev/null; then
+  os_version=$(lsb_release -c | awk '{print $2}' 2>/dev/null)  # Linux
+fi
