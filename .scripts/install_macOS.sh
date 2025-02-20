3e785b61782060d798ce045e7ba1f27120a2811f
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2025-02-19 21:57:18 -0800

    debug check

--- .scripts/install_macOS.sh
+++ .scripts/install_macOS.sh
@@ -10,6 +10,8 @@ fi
 
 ln -sf $DOTFILES_CONFIG/zsh/.zshenv $HOME/.zshenv
 
+echo "XDG_CONFIG_HOME is $XDG_CONFIG_HOME"
+
 ln -sf $DOTFILES_CONFIG/alacritty $XDG_CONFIG_HOME
 ln -sf $DOTFILES_CONFIG/karabiner $XDG_CONFIG_HOME
 ln -sf $DOTFILES_CONFIG/newsboat $XDG_CONFIG_HOME
