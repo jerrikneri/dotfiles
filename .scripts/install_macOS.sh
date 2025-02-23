673f629d1725df6f7cafe2b4ed340c87ce5e4239
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2025-02-22 18:50:29 -0800

    aerospace only

--- .scripts/install_macOS.sh
+++ .scripts/install_macOS.sh
@@ -1,7 +1,7 @@
 echo 'Starting MacOS install script...'
 
 # Homebrew
-if command -v brew &> /dev/null; then
+if command -v brew &>/dev/null; then
   echo 'Homebrew detected. Skipping install.'
 else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -22,7 +22,7 @@ ln -sf $DOTFILES_CONFIG/alacritty $XDG_CONFIG_HOME
 ln -sf $DOTFILES_CONFIG/karabiner $XDG_CONFIG_HOME
 ln -sf $DOTFILES_CONFIG/newsboat $XDG_CONFIG_HOME
 ln -sf $DOTFILES_CONFIG/nvim $XDG_CONFIG_HOME
-ln -sf $DOTFILES_CONFIG/skhd $XDG_CONFIG_HOME
+# ln -sf $DOTFILES_CONFIG/skhd $XDG_CONFIG_HOME
 ln -sf $DOTFILES_CONFIG/tmuxinator $XDG_CONFIG_HOME
 
 source $SCRIPTS/install_shared.sh
