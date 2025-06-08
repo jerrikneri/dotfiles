e5cf00e07b64407eee8aa8d355c0feb507297da5
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2025-06-08 11:21:59 -0700

    try adding DE

--- nix/modules/linux/system.nix
+++ nix/modules/linux/system.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  # services.xserver.enable = true;
+  services.xserver.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+
+  programs.alacritty.enable = true;
+
   # services.xserver.displayManager.sddm.enable = true;
   # services.xserver.desktopManager.plasma5.enable = true;
 }
