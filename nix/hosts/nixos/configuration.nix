801c4de3af07f176a10c80786e506f6f7a64d4b0
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2025-06-08 11:24:06 -0700

    move enable alacritty

--- nix/hosts/nixos/configuration.nix
+++ nix/hosts/nixos/configuration.nix
@@ -88,6 +88,7 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+  programs.alacritty.enabled = true;
   programs.neovim.enable = true;
   programs.zsh.enable = true;
 
--- nix/modules/linux/system.nix
+++ nix/modules/linux/system.nix
@@ -4,7 +4,7 @@
   services.xserver.enable = true;
   services.xserver.windowManager.i3.enable = true;
 
-  programs.alacritty.enable = true;
+  # programs.alacritty.enable = true;
 
   # services.xserver.displayManager.sddm.enable = true;
   # services.xserver.desktopManager.plasma5.enable = true;
