14858ad164537ce661b4273dfc834601308c40c4
Jerrik <jerrikcode+nix@gmail.com>
2025-06-10 21:58:44 -0700

    working projects

--- nix/modules/dev/php.nix
+++ nix/modules/dev/php.nix
@@ -1,14 +1,14 @@
-{ pkgs }:
+{ pkgs, extraBuildInputs ? [ ], extraShellHook ? "" }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
     php84Packages.composer
     php
     phpactor
-  ];
+  ] ++ extraBuildInputs;
 
 shellHook = ''
   echo "Php dev shell"
-'';
+'' + extraShellHook;
 }
 
--- nix/modules/dev/projects/tp.nix
+++ nix/modules/dev/projects/tp.nix
@@ -1,21 +1,21 @@
 { pkgs }:
 
-let
-  baseShell = import ../php.nix { inherit pkgs; };
-in
-  baseShell // {
-    buildInputs = baseShell.buildInputs ++ [
-      pkgs.lazycli
-      pkgs.lazyjournal
-      pkgs.podman
-    ];
+import ../php.nix {
+  inherit pkgs;
+  extraBuildInputs = with pkgs; [
+    lazycli
+    lazyjournal
+    podman
+  ];
 
-    shellHook = baseShell.shellHook + ''
-      echo "Project 1";
-      tpp;
-      sail up -d;
-      sail npm i && sail npm run dev -d;
-      n;
-    '';
-  }
+  extraShellHook = ''
+    echo "Project 1";
+    # Optional: comment these out if they’re too aggressive at shell startup
+    # tpp
+    # sail up -d
+    # sail npm i && sail npm run dev -d
+    # n
+  '';
+}
+
 
