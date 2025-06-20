177b70f9007ec0055b90bb65061be136741f824a
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2025-06-20 16:26:25 -0700

    missing semicolon

--- nix/hosts/nixos-pve/configuration.nix
+++ nix/hosts/nixos-pve/configuration.nix
@@ -33,7 +33,7 @@
     enable = true;
     version = 2;
     device = "dev/sda";
-  }
+  };
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
