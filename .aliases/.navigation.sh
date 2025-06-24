e9d44eae18147cb9e8e59a2856cda10a8c58e367
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2025-06-24 00:26:25 -0700

    add homepage

--- .aliases/.navigation.sh
+++ .aliases/.navigation.sh
@@ -1,3 +1,4 @@
 alias hl="cd $LAB"
+alias hp="cd $HOME/code/homepage"
 alias sb="cd $HOME/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/sb"
 alias write="cd $HOME/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/writing/writing"
