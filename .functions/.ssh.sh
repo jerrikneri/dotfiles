bdf3668f3ff0ca5a05e33e3b1f13d2551c10e774
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2025-06-24 00:04:49 -0700

    ssh function

---
+++ .functions/.ssh.sh
@@ -0,0 +1,23 @@
+s() {
+
+  case "$1" in
+  pve)
+    ssh $ADMIN_USER@$PVE_HOST
+    ;;
+  pve2)
+    ssh $ADMIN_USER@$PVE2_HOST
+    ;;
+  pbs)
+    ssh $ADMIN_USER@$PBS_HOST
+    ;;
+  hp)
+    ssh $ADMIN_USER@$HOMEPAGE_HOST
+    ;;
+  nix)
+    ssh $MY_USER@$NIX_HOST
+    ;;
+  *)
+    echo "Unsupported Host: $1"
+    ;;
+  esac
+}
