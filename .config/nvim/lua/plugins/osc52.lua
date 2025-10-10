3cec9988364331501b0290a45caf9acba1b6f59e
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2025-10-09 17:53:09 -0700

    nvim updates

--- .config/nvim/lua/plugins/osc52.lua
+++ .config/nvim/lua/plugins/osc52.lua
@@ -1,9 +1,7 @@
--- Not sure if this is really needed. Can use "+y to yank to + register
-return true;
 return {
   "ojroques/nvim-osc52",
   config = function()
-    require("osc52").setup {}
+    require("osc52").setup({})
     vim.api.nvim_create_autocmd("TextYankPost", {
       callback = function()
         if vim.v.event.operator == "y" and vim.v.event.regname == "" then
