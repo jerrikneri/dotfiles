0b7c89f87964441ae66c287d7c3b54e7ded93d0e
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2025-10-30 17:09:37 -0700

    mm shell

--- nix/flake.nix
+++ nix/flake.nix
@@ -113,7 +113,7 @@
         let
           pkgs = import nixpkgs { inherit system; };
           languages = [ "c" "csharp" "go" "node" "php" "python" "rust" ];
-          projects = [ "doom" "scripting" "tp" ];
+          projects = [ "doom" "mm" "scripting" "tp" ];
         in 
           nixpkgs.lib.genAttrs languages (lang:
             import ./modules/dev/${lang}.nix { inherit pkgs; }
