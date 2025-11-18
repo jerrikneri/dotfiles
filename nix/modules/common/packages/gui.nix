e4ea364121bff0eade017e62387fc8ea2da43ef3
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2025-11-18 00:38:34 -0800

    update

--- nix/modules/common/packages/gui.nix
+++ nix/modules/common/packages/gui.nix
@@ -4,6 +4,7 @@ let
   isLinux = pkgs.stdenv.isLinux;
 in {
   environment.systemPackages = with pkgs; [
+    sniffnet
   ] ++ lib.optionals pkgs.stdenv.isDarwin [
     # reserve for any GUI apps not in homebrew but available in nixpkgs
   ] ++ lib.optionals (!pkgs.stdenv.isAarch64) [
@@ -18,6 +19,7 @@ in {
     librewolf
     obsidian # allow unfree
     orca-slicer
+    opensnitch
     pavucontrol # GUI to manage audio in PulseAudio / Pipewire
     postman
     rofi
