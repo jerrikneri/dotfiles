bdb2b984b71691919bacff8e2293556eed77cc1c
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2026-02-08 19:46:27 -0800

    weechat

--- nix/modules/common/packages/tui.nix
+++ nix/modules/common/packages/tui.nix
@@ -11,6 +11,17 @@
     # newsboat # RSS TUI
     # posting # Postman TUI # broken package python3.13-textual-4.0.0
     spotify-player
+    (weechat.override {
+      configure = { availablePlugins, ... }: {
+        plugins = with availablePlugins; [
+          python
+          perl
+        ];
+        scripts = with pkgs.weechatScripts; [
+          wee-slack
+        ];
+      };
+    })
     yazi # File TUI
   ] ++ lib.optionals pkgs.stdenv.isDarwin [
     btop # htop / top alternative
